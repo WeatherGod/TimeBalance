fe7f34ce532f6b946a20a7cbb57463c652f434f5
Ben Root <ben.v.root@gmail.com>
2011-03-16 09:26:44 -0500

    Added a setup.py file to this project.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "TimeBalance",
+    version = "0.0.1",
+    author = "Benjamin Root",
+    author_email = "ben.v.root@gmail.com",
+    description = "An implementation of the TimeBalance task scheduler for weather radar adaptive sensing",
+    license = "BSD",
+    keywords = ("radar", "scanning", "scheduling", "adaptive sensing"),
+    url = "https://github.com/WeatherGod/TimeBalance",
+    #packages = ['TimeBalance',],
+    #package_dir = {'': 'lib'}
+    )
+
